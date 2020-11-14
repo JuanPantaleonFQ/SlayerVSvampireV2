6b288136327eb3237282078d4c0ed374a55f2e65
Juan Femenia Quevedo <53376393+JuanPantaleonFQ@users.noreply.github.com>
2020-11-14 20:02:11 +0100

    First commit.

--- SlayerVSvampireV2_/src/control/Command.java
+++ SlayerVSvampireV2_/src/control/Command.java
@@ -1,12 +1,18 @@
 package control;
 
-public class Command {
+public abstract class Command{
+	
+	
+	//ATRIBUTES:
 	protected final String name;
 	protected final String shortcut;
 	private final String details ;
 	private final String help;
 	protected static final String incorrectNumberOfArgsMsg = "Incorrect number of arguments";
 	protected static final String incorrectArgsMsg = "Incorrect arguments format";
+	
+	
+	//constructor:
 	public Command(String name, String shortcut, String details, String help){
 	this.name = name;
 	this. shortcut = shortcut;
@@ -15,17 +21,22 @@ public class Command {
 	}
 	public abstract boolean execute(Game game);
 	public abstract Command parse(String[] commandWords);
-	protected boolean matchCommandName(String name) {
-	return this.shortcut.equalsIgnoreCase(name) ||
-	this.name.equalsIgnoreCase(name);
+	protected boolean matchCommandName(String name){
+		return ((this.shortcut.equalsIgnoreCase(name)) ||	(this.name.equalsIgnoreCase(name)));
 	}
-	protected Command parseNoParamsCommand(String[] words) {
-	if (matchCommandName(words[0])) {
-	if (words.length != 1) {
-	System.err.println (incorrectArgsMsg);
-	return null;
+	
+	
+	protected Command parseNoParamsCommand(String[] words){
+		if (matchCommandName(words[0])){
+			if (words.length != 1) {
+				System.err.println (incorrectArgsMsg);
+				return null;
+				}
+				return this;
+		}
+			
 	}
-	return this;
+	
 
 
 }
